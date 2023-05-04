6b204d37cd1a97b2d41484e3d114177d685a3c23
EmmyC <esc@Emilys-MBP-2.attlocal.net>
2023-05-03 23:33:50 -0500

    Full Constructor test & Equals Method test

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -3,11 +3,9 @@ package org.launchcode.techjobs.oo.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.launchcode.techjobs.oo.CoreCompetency;
-import org.launchcode.techjobs.oo.Location;
-import org.launchcode.techjobs.oo.Job;
-import org.junit.Assert.assertNotEquals;
 import org.junit.Assert;
+import org.launchcode.techjobs.oo.*;
+
 
 
 /**
@@ -26,7 +24,44 @@ public class JobTest {
         Assert.assertNotEquals(spec, expected, actual);
     }
 
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job1 = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality " +
+                "control"), new CoreCompetency("Persistence"));
+        Assert.assertTrue("verify Job class", job1 instanceof Job);
+        Assert.assertEquals("verify job value", job1.getName(),
+                "Product tester");
+        Assert.assertTrue("verify employer is Employer class",
+                job1.getEmployer() instanceof Employer);
+        Assert.assertEquals("verify employer assigns Employer value", job1.getEmployer().getValue(),
+                "ACME");
+        Assert.assertTrue("verify location is Location class",
+                job1.getLocation() instanceof Location);
+        Assert.assertEquals("verify location assigns Location value", job1.getLocation().getValue(),
+                "Desert");
+        Assert.assertTrue("verify positionType is PositionType class",
+                job1.getPositionType() instanceof PositionType);
+        Assert.assertEquals("verify positionType assigns PositionType value",
+                job1.getPositionType().getValue(), "Quality " + "control");
+        Assert.assertTrue("verify coreCompetency is CoreCompetency class",
+                job1.getCoreCompetency() instanceof CoreCompetency);
+        Assert.assertEquals("verify coreCompetency assigns CoreCompetency value",
+                job1.getCoreCompetency().getValue(),
+                "Persistence");
 
+    }
 
+    @Test
+    public void testJobsForEquality() {
+        Job job1 = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        String spec = "test JobIds";
+        int expected = job1.getId();
+        int actual = job2.getId();
+        Assert.assertNotEquals(spec, expected, actual);
+    }
 
 }
